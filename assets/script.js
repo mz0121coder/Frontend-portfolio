2a5224ace670b9a89747c09450c1379b35434b1f
Mohammad Zafar <sz7861@outlook.com>
2021-07-29 13:00:37 +0100

    add preloader and slow fadeout

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,10 @@
 $(window).load(() => {
     windowLoaded();
   });
+
+// add preloader and slow fadeout
+const windowLoaded = () => {
+    $('.preloader')
+      .delay(400)
+      .fadeOut('slow');
+};
