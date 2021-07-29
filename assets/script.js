3520d3be717e6d21bc6fd13c67f9a4df91d1b39a
Mohammad Zafar <sz7861@outlook.com>
2021-07-29 13:03:18 +0100

    hide popup, only show it when a project is clicked

--- assets/script.js
+++ assets/script.js
@@ -8,7 +8,7 @@ const windowLoaded = () => {
     $('.preloader')
       .delay(400)
       .fadeOut('slow');
-      
+
 // set pop-up state to false
       const popUpElem = $('.popUp');
       const mainElem = $('main');
@@ -16,4 +16,8 @@ const windowLoaded = () => {
       const closePopUpBtn = $('#cancelBtn');  
       const projectElem = $('.project-item');
 
+    // hide pop-up element, only show it when a project is selected
+    $(popUpElem).hide(1);
+    mainElem.on('click', e => {
+      if ($(e.target).closest('.project-item').length > 0) {
 };
