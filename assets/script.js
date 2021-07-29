314520bb1923da3fc49e0f822f9af8c7d5b034b6
Mohammad Zafar <sz7861@outlook.com>
2021-07-29 13:01:52 +0100

    set pop-up state to false

--- assets/script.js
+++ assets/script.js
@@ -8,4 +8,12 @@ const windowLoaded = () => {
     $('.preloader')
       .delay(400)
       .fadeOut('slow');
+      
+// set pop-up state to false
+      const popUpElem = $('.popUp');
+      const mainElem = $('main');
+      let popUpState = false;
+      const closePopUpBtn = $('#cancelBtn');  
+      const projectElem = $('.project-item');
+
 };
